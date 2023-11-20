65ffcc3e77966cd1e9d0249921f7b6adbd936537
Sonu Yadav <85031957+sonuyadav5504@users.noreply.github.com>
2023-11-21 03:18:57 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,61 +1,73 @@
 import torch
-import numpy as np
 from torch_geometric.data import Data, DataLoader
-from torch_geometric.nn import GCNConv, global_mean_pool
+from torch_geometric.nn import GCNConv, global_max_pool
+from sklearn.metrics import roc_auc_score
 
-# Load data from CSV files
-node_features = torch.from_numpy(np.loadtxt('node_features.csv', delimiter=',', dtype=np.float64))
-edges = torch.from_numpy(np.loadtxt('edges.csv', delimiter=',', dtype=int))
-edge_features = torch.from_numpy(np.loadtxt('edge_features.csv', delimiter=',', dtype=np.float64))
-graph_labels = torch.from_numpy(np.loadtxt('graph_labels.csv', dtype=int))
-
-data_list = []
-for idx in range(len(graph_labels)):
-    start_node = sum(num_nodes[:idx])
-    end_node = start_node + num_nodes[idx]
-
-    start_edge = sum(num_edges[:idx])
-    end_edge = start_edge + num_edges[idx]
-
-    data = Data(
-        x=node_features[start_node:end_node],
-        edge_index=edges[start_edge:end_edge].t().contiguous(),
-        edge_attr=edge_features[start_edge:end_edge],
-        y=graph_labels[idx]
-    )
-    data_list.append(data)
-
-# DataLoader for classification
-loader = DataLoader(data_list, batch_size=32, shuffle=True)
-
-# Define your GNN model architecture for classification
-class MyGNNClassification(torch.nn.Module):
+# Define your GNN model architecture for binary classification
+class MyGNN(torch.nn.Module):
     def __init__(self):
-        super(MyGNNClassification, self).__init__()
-        self.conv1 = GCNConv(9, 64)
-        self.conv2 = GCNConv(64, 32)
-        self.out_layer = torch.nn.Linear(32, 1)  # Output for binary classification
+        super(MyGNN, self).__init__()
+        self.conv1 = GCNConv(num_features, 16)
+        self.bn1 = torch.nn.BatchNorm1d(16)
+        self.conv2 = GCNConv(16, 64)
+        self.bn2 = torch.nn.BatchNorm1d(64)
+        self.dropout = torch.nn.Dropout(0.5)
+        self.conv3 = GCNConv(64, 64)  # Adjust the output size to match the target size
+        self.bn3 = torch.nn.BatchNorm1d(64)
+        self.out_layer = torch.nn.Linear(64, 32)  # Output layer for binary classification
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index)
+        x = self.bn1(x)
         x = torch.relu(x)
         x = self.conv2(x, edge_index)
+        x = self.bn2(x)
         x = torch.relu(x)
-        x = global_mean_pool(x, batch=None)
-        x = torch.sigmoid(self.out_layer(x)).squeeze(1)  # Sigmoid for binary classification
+        x = self.dropout(x)
+        x = self.conv3(x, edge_index)
+        x = self.bn3(x)
+        x = torch.relu(x)
+        x = global_max_pool(x, batch=None)
+        x = self.dropout(x)
+        x = self.out_layer(x)  # Adjust the shape for binary classification
         return x
 
-# Instantiate the model
-model = MyGNNClassification()
+# Initialize your GNN model
+model = MyGNN()  
 
-# Train your classification model
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-criterion = torch.nn.BCELoss()  # Binary Cross Entropy loss for classification
+# Define optimizer and loss function
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+criterion = torch.nn.BCEWithLogitsLoss()  # Binary Cross Entropy loss for binary classification
 
-# Example usage of the DataLoader
-for data in loader:
-    optimizer.zero_grad()
-    output = model(data.x, data.edge_index)
-    loss = criterion(output, data.y.float())  # Convert data.y to float for BCELoss
-    loss.backward()
-    optimizer.step()
+# Assuming graph_labels are binary (0 or 1)
+# graph_labels_c = graph_labels_c.float()  # Convert labels to float for BCEWithLogitsLoss
+
+# Training loop
+train_losses = []
+valid_losses = []
+best_loss = float('inf')
+num_epochs = 30  # Set your desired number of epochs
+l=len(loader_c)
+for epoch in range(num_epochs):
+    model.train()
+    epoch_loss = 0.0
+    for data in loader_c:
+        if i>len(loader_c):
+            optimizer.zero_grad()
+            out = model(data.x.float(), data.edge_index)
+            print("out ",out.squeeze().size())
+            print("data ",data.y.size())
+            loss = criterion(out.squeeze(), data.y.float())  # Use data.y for graph labels
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+
+    epoch_loss /= len(loader_c)
+    train_losses.append(epoch_loss)
+    print(f'Epoch [{epoch+1}/{num_epochs}], Train Loss: {epoch_loss:.4f}')
+# # Evaluation
+# model.eval()
+# with torch.no_grad():
+#     predictions = model(data.x.float(), data.edge_index)
+#     roc_auc = roc_auc_score(graph_labels.numpy(), torch.sigmoid(predictions.squeeze()).numpy())
+#     print(f'ROC-AUC: {roc_auc:.4f}')
